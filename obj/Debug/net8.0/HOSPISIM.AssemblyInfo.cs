316c7b2bb066c425cad875d5d61aec4303b7d848
Jeezyel <jeezyelpereira@gmail.com>
2025-06-12 20:06:17 -0300

    ADD a parte de uml

--- obj/Debug/net8.0/HOSPISIM.AssemblyInfo.cs
+++ obj/Debug/net8.0/HOSPISIM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HOSPISIM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2129a74b49f563fa61c6d725b71cabf728e1a0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c83bdcbe11f5f7cf3a4fcf387bb2f85c166f79")]
 [assembly: System.Reflection.AssemblyProductAttribute("HOSPISIM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HOSPISIM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
